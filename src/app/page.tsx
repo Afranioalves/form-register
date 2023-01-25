74331783864630d62b43e57d04d494c9270851b8
Afranio Alves <afranio777alves@gmail.com>
2023-01-25 18:01:06 +0100

    configuração inicial do projecto

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import styles from './styles/page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.container}>
+    </main>
+  )
+}
